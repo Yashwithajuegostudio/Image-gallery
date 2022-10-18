762ae3aa19eaebe5e247d9424a657dd7346ac342
Yashwitha <yashwitha@juegostudioz.com>
2022-10-18 16:56:13 +0530

    FIX:Feedback changes are updated

--- js/index.js
+++ js/index.js
@@ -1,18 +1,13 @@
 const images = document.querySelectorAll(".image-row img");
 
-const mainImage = document.getElementById('mainimg');
+const mainImage = document.getElementById("mainimg");
 
-
-
-for (items of images) {
-    items.addEventListener('click', (e) => {
-        images.forEach(element => {
-            element.classList.remove("active");
-
-        });
-        mainImage.src = e.target.src;
-        e.target.classList.add("active");
-
+for (image of images) {
+  image.addEventListener("click", (e) => {
+    images.forEach((element) => {
+      element.classList.remove("active");
     });
+    mainImage.src = e.target.src;
+    e.target.classList.add("active");
+  });
 }
-

d6ce16aba7c56f8b821c578779dc88f561db5dca
Yashwitha <yashwitha@juegostudioz.com>
2022-10-21 17:35:25 +0530

    FEAT:Breed dog list display API integration

--- js/displayData.js
+++ js/displayData.js
@@ -1,12 +1,18 @@
+import { getBreedImageApiData } from "./dogBreed";
+
 function showDropDownData(arr) {
   for (var i = 0; i < arr.length; i++)
-    if (Array.isArray(arr[i])) showDropDownData(arr[i]);
-    else {
+    if (Array.isArray(arr[i])) {
+      showDropDownData(arr[i]);
+    } else {
       const selectList = document.querySelector("#selectList");
-      const nodes = arr.map((arr) => {
+      const nodes = arr.map((arr, i) => {
         const option = document.createElement("option");
         option.className = "option-value";
         option.textContent = arr;
+        option.addEventListener("click", () => {
+          getBreedImageApiData(option.value);
+        });
         return option;
       });
 
--- js/dogBreed.js
+++ js/dogBreed.js
@@ -2,21 +2,34 @@ import { getApiCall } from "./apiHelper";
 import { showDropDownData } from "./displayData";
 import { showDropDown } from "./index";
 
-const getUrl = "https://dog.ceo/api/breeds/list/all";
+const dogBreedNameUrl = "https://dog.ceo/api/breeds/list/all";
 const getData = (data) => {
-  show(data);
+  displayBreedName(data);
 };
-getApiCall(getUrl, getData);
+getApiCall(dogBreedNameUrl, getData);
 
-function show(data) {
-  const arr = Object.values(data);
-  const arrayObject = Object.values(arr[0]);
-  const filteredValue = arrayObject.filter((message, i) => {
-    if (message.length != 0) {
-      return message;
+function displayBreedName(data) {
+  const breedNameArray = [];
+  Object.keys(data.message).forEach((key) => {
+    if (data.message[key].length > 0) {
+      breedNameArray.push(key);
     }
   });
-  showDropDownData(filteredValue);
+  showDropDownData(breedNameArray);
+}
+// display drop down
+showDropDown();
+
+function getBreedImageApiData(breedName) {
+  const dogBreedImageUrl = `https://dog.ceo/api/breed/${breedName}/images`;
+  const getData = (data) => {
+    displayImageData(data);
+  };
+  getApiCall(dogBreedImageUrl, getData);
 }
 
-showDropDown();
+function displayImageData(data) {
+  const breedImageArray = [];
+  let arr = Object.entries(data.message);
+}
+export { getBreedImageApiData };
